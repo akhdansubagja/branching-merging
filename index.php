f39bc0ad8ebf60ce9f0e29bb76e9761ada907871
akhdansubagja <if23.akhdansubagja@mhs.ubpkarawang.ac.id>
2024-11-28 19:32:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/lux/bootstrap.min.css" integrity="sha384-9+PGKSqjRdkeAU7Eu4nkJU8RFaH8ace8HGXnkiKMP9I9Te0GJ4/km3L1Z8tXigpG" crossorigin="anonymous">
 </head>
 <body>
     <h1>login</h1>
@@ -13,7 +12,6 @@
         <input type="password" name="password" placeholder="masukkan password anda">
         <input type="submit" value="login" name="submit">
     </form>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/lux/bootstrap.min.css" integrity="sha384-9+PGKSqjRdkeAU7Eu4nkJU8RFaH8ace8HGXnkiKMP9I9Te0GJ4/km3L1Z8tXigpG" crossorigin="anonymous"></script>
+    
 </body>
 </html>
