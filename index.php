a0b9b0c929debb50d7432854c4053a7b5bc5ab24
akhdansubagja <if23.akhdansubagja@mhs.ubpkarawang.ac.id>
2024-11-28 19:20:12 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/lux/bootstrap.min.css" integrity="sha384-9+PGKSqjRdkeAU7Eu4nkJU8RFaH8ace8HGXnkiKMP9I9Te0GJ4/km3L1Z8tXigpG" crossorigin="anonymous">
+</head>
+<body>
+    <h1>login</h1>
+    <form action="./backend/login.php" method="post">
+        <input type="email" name="email" placeholder="masukkan email anda">
+        <input type="password" name="password" placeholder="masukkan password anda">
+        <input type="submit" value="login" name="submit">
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/lux/bootstrap.min.css" integrity="sha384-9+PGKSqjRdkeAU7Eu4nkJU8RFaH8ace8HGXnkiKMP9I9Te0GJ4/km3L1Z8tXigpG" crossorigin="anonymous"></script>
+</body>
+</html>
