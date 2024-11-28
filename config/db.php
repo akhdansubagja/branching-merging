16b20b385cb954b15dbe342eb82f3cf3ba28bc8d
akhdansubagja <if23.akhdansubagja@mhs.ubpkarawang.ac.id>
2024-11-28 19:35:31 +0700

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+$DBHOST = 'localhost';
+$DBUSER = 'root';
+$DBPASSWORD = '';
+$DBNAME = 'pemweb-db';
+$port = 3307;          // Custom MySQL port (as you mentioned 3307)
+
+
+$db_connect = mysqli_connect($DBHOST,$DBUSER,$DBPASSWORD,$DBNAME, $port);
+
+if(mysqli_connect_errno()){
+    echo "failed connect to mysql ".mysqli_connect_error(); 
+}
+
